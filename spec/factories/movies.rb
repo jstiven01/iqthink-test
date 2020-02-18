7c216b53e446257c3c057d9c519a8c5418fcdc98
jstiven01 <stiven10@gmail.com>
2020-02-18 17:26:10 -0500

    Creating movie api

---
+++ spec/factories/movies.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+    factory :movie do
+      title { Faker::Book.title }
+      overview {Faker::Movie.quote} 
+      votes { rand(2500) }
+      poster_url { UiFaces.face }
+      release_date { Faker::Date.between(from: 15.days.ago, to: Date.today) }
+    end
+  end
---
+++ spec/requests/movies_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe 'Movie API', type: :request do
+  # initialize test data 
+  let!(:movies) { create_list(:movie, 10) }
+  let(:movie_id) { movies.first.id }
+
+  # Test suite for GET /movies
+  describe 'GET /movies' do
+    # make HTTP get request before each example
+    before { get '/movies' }
+
+    it 'returns movies' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /movies/:id
+  describe 'GET /movies/:id' do
+    before { get "/movies/#{movie_id}" }
+
+    context 'when the record exists' do
+      it 'returns the movie' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(movie_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:movie_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Movie/)
+      end
+    end
+  end
+
+  # Test suite for POST /movies
+  describe 'POST /movies' do
+    # valid payload
+    let(:valid_attributes) { { title: 'Learn Elm', overview: 'Lorem Ipsum',
+        votes: 52, poster_url:"http//fake.url", release_date: '2020-01-01' } }
+
+    context 'when the request is valid' do
+      before { post '/movies', params: valid_attributes }
+
+      it 'creates a movie' do
+        expect(json['title']).to eq('Learn Elm')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/movies', params: { title: 'Foobar',
+        votes: 52, poster_url:"http//fake.url", release_date: '2020-01-01' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Overview can't be blank/)
+      end
+    end
+  end
+
+
+
+end
