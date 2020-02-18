ea510a30faedb5473cfba815dc674a1221d7c8d8
jstiven01 <stiven10@gmail.com>
2020-02-18 16:57:44 -0500

    TDD adding model movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ApplicationRecord
+    validates_presence_of :title, :overview, :votes, :poster_url, :release_date
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:overview) }  
+  it { should validate_presence_of(:votes) }
+  it { should validate_presence_of(:poster_url) }
+  it { should validate_presence_of(:release_date) }
 end
